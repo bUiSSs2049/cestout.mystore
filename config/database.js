5a281055d7424a5e8518b932f6571672a3d985a2
aissa <89082595+bUiSSs2049@users.noreply.github.com>
2023-04-21 09:50:15 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 module.exports = {
  'connection':{
-  'host':'localhost',
+  'host':'getfollowers2.online',
   'user':'u909908174_Aissa_db_sql__',
   'password':'Mysql1/2/3.db'
  },
